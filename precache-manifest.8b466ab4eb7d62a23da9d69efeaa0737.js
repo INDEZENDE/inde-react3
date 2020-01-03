ba36e82e782013cf75ecc9c60c9520b2c074578d
Inde Rios <i.rios.zendejas@gmail.com>
2020-01-03 00:25:04 -0600

    prueba deploy

---
+++ precache-manifest.8b466ab4eb7d62a23da9d69efeaa0737.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd90eb70906e437018c90696814da88f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f377a6404e3671f870c9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "50e132cd0306b45393cf",
+    "url": "/static/js/2.c4346f55.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c4346f55.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f377a6404e3671f870c9",
+    "url": "/static/js/main.698ba65d.chunk.js"
+  },
+  {
+    "revision": "88284f1f1cb6ffbdb56c",
+    "url": "/static/js/runtime-main.0a597960.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
